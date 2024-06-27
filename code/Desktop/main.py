f92a2f953601249bd0a422f296739a62bd69473d
Stephen Collins <scrivera64@gmail.com>
2024-06-27 12:19:03 -0600

    start work on connection ui and backend

--- code/Desktop/main.py
+++ code/Desktop/main.py
@@ -3,6 +3,33 @@ from tkinter import ttk
 
 from spike import SpikeMessenger
 
+class ConnectionWindow(Toplevel):
+    """ connection window for connecting to the correct robot """
+    def __init__(self, parent, spike_messenger):
+        super(ConnectionWindow, self).__init__(parent)
+        self.title("Connect")
+        self.geometry("300x200")
+
+        self.spike_messenger = spike_messenger
+
+        main_frame = ttk.Frame(self, padding=10)
+        main_frame.grid()
+
+        self.listbox = Listbox(main_frame)
+        self.listbox.grid(column=0, row=0)
+
+        self.status = Label(main_frame)
+        self.status.grid(column=0, row=1)
+
+    def scan(self, *args):
+        self.status.configure(text="Scanning...")
+
+        def process_results(results):
+            for i, result in enumerate(results):
+                self.listbox.insert(i + 1, result)
+
+        self.spike_messenger.scan(process_results)
+
 class MainWindow(Tk):
     """ main window for controlling the robot """
     def __init__(self):
@@ -36,6 +63,9 @@ class MainWindow(Tk):
 
     def move_right(self, *args):
         self.spike_messenger.move(100, 100)
+
+    def open_connection_window(self):
+        ConnectionWindow(self, self.spike_messenger)
 
 if __name__ == "__main__":
     main_window = MainWindow()
--- code/Desktop/spike.py
+++ code/Desktop/spike.py
@@ -1,13 +1,37 @@
 import asyncio
+import threading
 # from bleak import BleakClient, BleakScanner
 # from bleak.backends.characteristic import BleakGATTCharacteristic
 # from bleak.backends.device import BLEDevice
 # from bleak.backends.scanner import AdvertisementData
 
-class SpikeMessenger(object):
-	def __init__(self):
-		pass
+SCAN_TIMEOUT = 10.0
 
-	def move(self, speed, direction):
-		print(f'moving in direction {direction} at speed {speed}') 
-		
+SERVICE_UUID = "0000fd02-0000-1000-8000-00805f9b34fb"
+
+class SpikeMessenger(object):
+    def __init__(self):
+        pass
+        # device = await BleakScanner.find_device_by_filter(filterfunc=self.service_uuid_is_match, timeout=SCAN_TIMEOUT)
+
+    def move(self, speed, direction):
+        print(f'moving in direction {direction} at speed {speed}')
+
+    # def service_uuid_is_match(self, device: BLEDevice, adv: AdvertisementData) -> bool:
+    #     return SERVICE_UUID.lower() in adv.service_uuids
+
+    def scan(self, callback):
+        thread = threading.Thread(target=self._scan, args=(callback,))
+        thread.start()
+
+        # asyncio.to_thread(self._scan())
+
+    def _scan(self, callback):
+        with BleakScanner(detection_callback=callback) as scanner:
+            for i in range(SCAN_TIMEOUT):
+                sleep(1)
+
+            # devices = scanner.get_discovered_devices(timeout=SCAN_TIMEOUT)
+
+        # return devices
+
